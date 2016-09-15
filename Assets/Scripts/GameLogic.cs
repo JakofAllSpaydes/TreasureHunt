d5ff33522346e503a8938df8b5ccfaf082361698
Jack Guo <yiming7391@gmail.com>
2016-09-15 16:44:44 -0400

    Player movement, End goal w/ textbox. No landmarks

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	bool didPlayerWin = false;
+	public Transform player;
+	public Transform treasure;
+	public Text textbox;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (didPlayerWin == true) {
+			
+			textbox.text = "You got the treasure!";
+
+		}else if ((treasure.position - player.position).magnitude < 3f){
+			
+			textbox.text = "Press [SPACE] to get treasure!";
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				didPlayerWin = true;
+			}
+	}
+}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			
+			//move object, frame independent
+			transform.position += new Vector3 (0, 0, 7) * Time.deltaTime;
+
+			//rotate object to face the way its moving
+			//transform.eulerAngles = new Vector3 (0, 0, 0); 
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			
+			//move object, frame independent
+			transform.position += new Vector3 (0, 0, -7) * Time.deltaTime;
+
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+
+			//move object, frame independent
+			transform.position += new Vector3 (-7, 0, 0) * Time.deltaTime;
+
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+
+			//move object, frame independent
+			transform.position += new Vector3 (7, 0, 0) * Time.deltaTime;
+
+		}
+
+		Camera.main.transform.position = transform.position + new Vector3 (0, 0, 0); //(0, 4, -3
+
+	}
+}
